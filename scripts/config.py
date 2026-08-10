3533a8ba568acc3d521ef8d57760c7a2b3f91a45
Midhunraj <midhunraj303@gmail.com>
2026-08-10 08:47:32 +0530

    Add configuration and logger

--- scripts/config.py
+++ scripts/config.py
@@ -3,3 +3,27 @@
 # Read YAML
 # Return Python dictionary
 # Validate configuration (later)
+
+import yaml
+from pathlib import Path
+
+def load_config(config_path: str) -> dict:
+    """
+    Load configuration from a YAML file
+    args -> config_path: path to the YAML configuration file
+    returns: configuration as a dictionary
+    """
+
+    config_file = Path(config_path)
+
+    if not config_file.exists():
+        raise FileNotFoundError(
+            f"Configuration file not found: {config_path}"
+        )
+
+    with open(config_file,"r",encoding="utf-8") as file:
+        config= yaml.safe_load(file)        # safe_load() is the recommended choice 
+                                            # because it only loads standard YAML data types 
+                                            # and avoids executing arbitrary Python objects from the file.
+
+    return config
--- scripts/logger.py
+++ scripts/logger.py
@@ -4,3 +4,49 @@
 # Write logs to file
 # Print logs to console
 
+import logging
+from pathlib import Path
+from scripts.config import load_config
+
+def setup_logger() -> logging.Logger:
+    """
+    Configure and return the project logger
+    """
+
+    config = load_config("config/config_local.yaml")
+
+    log_file = config["logging"]["log_file"]
+    log_level = config["logging"]["level"]
+
+    # create log directory if it doesn't exists
+    # it works regardless of the folder name
+    # it doesn't fail if the directory already exists.
+    Path(log_file).parent.mkdir(parents=True,exist_ok=True)
+
+    logger = logging.getLogger("news_etl")
+
+    #prevent duplicate handlers
+    if logger.hasHandlers():
+        return logger
+
+    logger.setLevel(getattr(logging,log_level.upper()))
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)-8s | %(message)s",
+         datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    #console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    #File Handler
+    file_handler =logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
+
+logger = setup_logger()
