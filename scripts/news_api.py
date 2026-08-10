25438db417285142059474cf594dbc7d669fa752
Midhunraj <midhunraj303@gmail.com>
2026-08-10 09:52:06 +0530

    Implement newsAPI client configuration

--- scripts/news_api.py
+++ scripts/news_api.py
@@ -6,3 +6,54 @@
 # Retry
 # Rate limit handling
 
+import requests
+from scripts.logger import logger
+
+def build_url(config: dict) -> str:
+    """
+    Return the newsAPI endpoint URL
+    args:
+        config: project configuration dictionary
+    returns: newsAPI endpoint URL
+    """
+
+    logger.info("Building NewsAPI URL")
+    return config["newsapi"]["base_url"]
+
+
+def build_headers(config: dict) -> dict:
+    """
+    Build HTTP request headers
+    args:
+        config: project configuration dictionary
+    
+    returns: Dictionary containing HTTP headers
+    """
+
+    logger.info("Building request headers")
+
+    return{
+        "X-Api-Key" : config["newsapi"]["api_key"]
+    }
+
+
+def build_query_params(config: dict) -> dict:
+    """
+    Build query parameters for the NewsAPI request
+    args:
+        config: Project configuration dictionary
+    
+    Returns: Dictionary containing query parameters
+    """
+
+    logger.info("Building query parameters")
+
+    news_config = config["newsapi"]
+
+    return {
+        "q" : news_config["query"],
+        "language" : news_config["language"],
+        "pageSize" : news_config["page_size"]
+    }
+
+
